9ed9cd59b6ceda94c830eef5e7c6438317b6478e
EvertKa <evert.karp@ametikool.ee>
2023-09-21 14:45:21 +0300

    vue

---
+++ src/App.vue
@@ -0,0 +1,83 @@
+<template>
+    <Disclosure>
+      <DisclosureButton class="py-2 w-full">
+        BIOLAGUNEVAD KÖÖGIJÄÄTMED
+      </DisclosureButton>
+      <DisclosurePanel class="text-gray-500">
+        Yes! You can purchase a license that you can share with your entire team.
+      </DisclosurePanel>
+    </Disclosure>
+    <Disclosure>
+      <DisclosureButton class="py-2 w-full">
+        BIOLAGUNEVAD AIAJÄÄTMED
+      </DisclosureButton>
+      <DisclosurePanel class="text-gray-500">
+        Yes! You can purchase a license that you can share with your entire team.
+      </DisclosurePanel>
+    </Disclosure>
+    <Disclosure>
+      <DisclosureButton class="py-2 w-full">
+        KLAASPAKEND
+      </DisclosureButton>
+      <DisclosurePanel class="text-gray-500">
+        Yes! You can purchase a license that you can share with your entire team.
+      </DisclosurePanel>
+    </Disclosure>
+    <Disclosure>
+      <DisclosureButton class="py-2 w-full">
+        VANAPABER
+      </DisclosureButton>
+      <DisclosurePanel class="text-gray-500">
+        Yes! You can purchase a license that you can share with your entire team.
+      </DisclosurePanel>
+    </Disclosure>
+    <Disclosure>
+      <DisclosureButton class="py-2 w-full">
+        KASUTUSKÕLBMATU TEKSTIIL
+      </DisclosureButton>
+      <DisclosurePanel class="text-gray-500">
+        Yes! You can purchase a license that you can share with your entire team.
+      </DisclosurePanel>
+    </Disclosure>
+    <Disclosure>
+      <DisclosureButton class="py-2 w-full">
+        KASUTUSKÕLBLIK TEKSTIIL
+      </DisclosureButton>
+      <DisclosurePanel class="text-gray-500">
+        Yes! You can purchase a license that you can share with your entire team.
+      </DisclosurePanel>
+    </Disclosure>
+    <Disclosure>
+      <DisclosureButton class="py-2 w-full">
+        OHTLIKUD JÄÄTMED
+      </DisclosureButton>
+      <DisclosurePanel class="text-gray-500">
+        Yes! You can purchase a license that you can share with your entire team.
+      </DisclosurePanel>
+    </Disclosure>
+    <Disclosure>
+      <DisclosureButton class="py-2 w-full">
+        PLAST- JA METALLPAKEND
+      </DisclosureButton>
+      <DisclosurePanel class="text-gray-500">
+        Yes! You can purchase a license that you can share with your entire team.
+      </DisclosurePanel>
+    </Disclosure>
+    <Disclosure>
+      <DisclosureButton class="py-2 w-full">
+        PABER- JA KARTONGPAKED
+      </DisclosureButton>
+      <DisclosurePanel class="text-gray-500">
+        Yes! You can purchase a license that you can share with your entire team.
+      </DisclosurePanel>
+    </Disclosure>
+  </template>
+  
+  <script setup>
+    import {
+      Disclosure,
+      DisclosureButton,
+      DisclosurePanel,
+    } from '@headlessui/vue'
+
+  </script>
